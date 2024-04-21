530e2d4b70b2e30fa4f76aaf9ff697b8d43886bb
EmiHarcho <nazirovemi@gmail.com>
2024-04-21 15:03:17 +0300

    fix_sing_to_page

--- frontend/src/components/Pages/Page/MenuItems.jsx
+++ frontend/src/components/Pages/Page/MenuItems.jsx
@@ -4,38 +4,42 @@ import { FiSettings } from 'react-icons/fi';
 import PageUniver from './University/PageUniver';
 import SettingsUser from './SettingsUser';
 
+let token = localStorage.getItem('authToken')
 const MenuItems = () => {
     const [activeComponent, setActiveComponent] = useState('university');
 
     const handleComponentChange = (component) => {
       setActiveComponent(component);
     }
-  return (
-    <div>
-       <nav className='flex justify-between p-6 border-b-2 border-[#9e9a9a]'>
-
-        <div className='pt-2 text-[25px] text-darkBlue font-bold'>
-        Xaxathon
-        </div>
-            <div>
-            <div className='flex'>
-            <div className={`flex items-center p-2 mt-2 rounded-lg  text-lg ${activeComponent === 'university' ? 'text-blue-600 bg-blue-100' : 'hover:bg-gray-100'}`} onClick={() => handleComponentChange('university')}>
-              <AiOutlineTeam className="mr-3 " />
-              Университеты
-            </div>
-            <div className={`flex items-center p-2 mt-2 rounded-lg cursor-pointer text-lg ${activeComponent === 'settings' ? 'text-blue-600 bg-blue-100' : 'hover:bg-gray-100'}`} onClick={() => handleComponentChange('settings')}>
-              <FiSettings className="mr-3 " />
-              Профиль
-            </div>
-        </div>
-            </div>
+    console.log(token);
+  if (token) {
+    return (
+      <div>
+         <nav className='flex justify-between p-6 border-b-2 border-[#9e9a9a]'>
+  
+          <div className='pt-2 text-[25px] text-darkBlue font-bold'>
+          Xaxathon
+          </div>
+              <div>
+              <div className='flex'>
+              <div className={`flex items-center p-2 mt-2 rounded-lg  text-lg ${activeComponent === 'university' ? 'text-blue-600 bg-blue-100' : 'hover:bg-gray-100'}`} onClick={() => handleComponentChange('university')}>
+                <AiOutlineTeam className="mr-3 " />
+                Университеты
+              </div>
+              <div className={`flex items-center p-2 mt-2 rounded-lg cursor-pointer text-lg ${activeComponent === 'settings' ? 'text-blue-600 bg-blue-100' : 'hover:bg-gray-100'}`} onClick={() => handleComponentChange('settings')}>
+                <FiSettings className="mr-3 " />
+                Профиль
+              </div>
+          </div>
+              </div>
+              
+          </nav>
+          {activeComponent === 'university' && <PageUniver />}
+          {activeComponent === 'settings' && <SettingsUser />}
+      </div>
             
-        </nav>
-        {activeComponent === 'university' && <PageUniver />}
-        {activeComponent === 'settings' && <SettingsUser />}
-    </div>
-          
-  );
+    );
+  }
 };
 
 export default MenuItems;
